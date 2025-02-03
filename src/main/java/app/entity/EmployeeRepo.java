d2c3648c0b5a53a2a52b75b5a21a4cab5bc02c21
CodeFrit <casiobake@gmail.com>
2025-02-03 20:16:09 +0300

    Update EmployeeRepo.java

--- src/main/java/app/entity/EmployeeRepo.java
+++ src/main/java/app/entity/EmployeeRepo.java
@@ -1,6 +1,7 @@
 package app.entity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-
+	List<Employee> findByFname(String fname);
 }
